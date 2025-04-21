00601b8043e8f57761ac1dad144366f8e4eee57b
SHARATHKUMAR0112 <swhastik@gmail.com>
2025-04-21 11:36:25 +0000

    added testcase 9 template

---
+++ Project/src/test/java/pages/SportsJamPumaWomenSportsPage.java
@@ -0,0 +1,145 @@
+package pages;
+
+import utils.WebDriverHelper;
+
+public class SportsJamPumaWomenSportsPage {
+
+    WebDriverHelper webDriverHelper;
+
+    public SportsJamPumaWomenSportsPage()
+    {
+        
+    }
+
+    public void runWomenSportsCase()
+    {
+        verifyHomePageURL();
+        
+        clickOnBrand();
+
+        verifyBrandsPageURL();
+
+        clickOnSearchBar();
+
+        sendDataToSearchBar();
+
+        clickOnSearchIcon();
+
+        verifyURLForPuma();
+
+        clickOnPuma();
+
+        verifyThePumaPage();
+
+        clickOnAccesories();
+
+        verifyAccessoiresTextInFilter();
+
+        hoverOverPumaWomenSportQuartersAssorted();
+
+        clickOnQuickView();
+
+        switchToIframe();
+
+        verifyIFrameURL();
+
+        clickOnWishList();
+
+        verifyLoginPromptMessage();
+
+        clickAndSendDataToEmail();
+
+        clickAndSendDataToPassword();
+
+        clickOnSignIn();
+
+        verifyTheErrorMessage();
+    }
+
+    private void verifyHomePageURL() {
+
+    }
+
+    private void clickOnBrand() {
+
+    }
+
+    private void verifyBrandsPageURL() {
+
+    }
+
+    private void clickOnSearchBar() {
+
+    }
+
+    private void sendDataToSearchBar() {
+
+    }
+
+    private void clickOnSearchIcon() {
+
+    }
+
+    private void verifyURLForPuma() {
+
+    }
+
+    private void clickOnPuma() {
+
+    }
+
+    private void verifyThePumaPage() {
+
+    }
+
+    private void clickOnAccesories() {
+
+    }
+
+    private void verifyAccessoiresTextInFilter() {
+
+    }
+
+    private void hoverOverPumaWomenSportQuartersAssorted() {
+
+    }
+
+    private void clickOnQuickView() {
+
+    }
+
+    private void switchToIframe() {
+
+    }
+
+    private void verifyIFrameURL() {
+
+    }
+
+    private void clickOnWishList() {
+
+    }
+
+    private void verifyLoginPromptMessage() {
+
+    }
+
+    private void clickAndSendDataToEmail() {
+
+    }
+
+    private void clickAndSendDataToPassword() {
+
+    }
+
+    private void clickOnSignIn() {
+
+    }
+
+    private void verifyTheErrorMessage() {
+
+    }
+
+
+
+}
