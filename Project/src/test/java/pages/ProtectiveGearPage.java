a574e78725b02e7a3b87640e04182677c7fcbc06
murali413 <dmuralidhar413@gmail.com>
2025-04-22 04:30:00 +0000

    gearpage and shops page added

--- Project/src/test/java/pages/ProtectiveGearPage.java
+++ Project/src/test/java/pages/ProtectiveGearPage.java
@@ -1,8 +1,11 @@
 package pages;
 
+import org.testng.Assert;
+
 import com.aventstack.extentreports.ExtentTest;
 
 import uistore.ProtectiveGearLocators;
+import uistore.ShopSportsLocators;
 import utils.Base;
 import utils.WebDriverHelper;
 
@@ -21,26 +24,118 @@ public class ProtectiveGearPage {
     public void clickOnBattingGloves(){
         helper.clickOnElement(ProtectiveGearLocators.battingGloves, "Clicked On Batting Gloves");
     }
-    public void verifyBattingGloves(){
-
-    }
     public void verifyBattingGlovesURL(){
-        
+        String data= Base.driver.getCurrentUrl();
+        try {
+            Assert.assertTrue(data.contains("/batting-gloves-cricket-shop-online-india"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void verifyBattingGloves(){
+        try {
+            String data=helper.getText(ProtectiveGearLocators.verifyWicketKeepingLegGuard, "Verified ");
+            Assert.assertEquals(data, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     // 
     public void clickOnBattingLegGuards(){
         helper.clickOnElement(ProtectiveGearLocators.BattingegGuards, "Clicked On Batting Leg Guards");
     }
     public void verifyBattingLeguardURL(){
-
+        String data= Base.driver.getCurrentUrl();
+        try {
+            Assert.assertTrue(data.contains("/batting-leg-guards-shop-online-india"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public void verifyBattingLegGuard(){
-
+        try {
+            String data=helper.getText(ProtectiveGearLocators.verifyWicketKeepingLegGuard, "Verified ");
+            Assert.assertEquals(data, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 // 
     public void clickOnWicketKeepingGloves(){
         helper.clickOnElement(ProtectiveGearLocators.wicketKeepingGloves, "Clicked On Wicket Keeping Gloves");
     }
-
+    public void verifyWicketKeepingGlovesURL(){
+        String data= Base.driver.getCurrentUrl();
+        try {
+            Assert.assertTrue(data.contains("/wicket-keeping-gloves-cricket-shop-online-india"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void verifyWicketKeepingGloves(){
+        try {
+            String data=helper.getText(ProtectiveGearLocators.verifyWicketKeepingLegGuard, "Verified ");
+            Assert.assertEquals(data, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void clickoNWicketKeepingLegGuard(){
+        helper.clickOnElement(ProtectiveGearLocators.wicketKeepingLegGuards, "Clicked On WicketKeepingLegGuard");
+    }
+    public void verifyWicketKeepingLegGuardURL(){
+        String data= Base.driver.getCurrentUrl();
+        try {
+            Assert.assertTrue(data.contains("/wicket-keeping-leg-guards-shop-online-india"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void verifyWicketKeepingLegGuard(){
+        try {
+            String data=helper.getText(ProtectiveGearLocators.verifyWicketKeepingLegGuard, "Verified ");
+            Assert.assertEquals(data, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void clickonCricketHelmets(){
+        helper.clickOnElement(ProtectiveGearLocators.cricketHelmets, "Clicked On Cricket Helmets");
+    }
+    public void verifyCricketHelmetsURL(){
+        String data= Base.driver.getCurrentUrl();
+        try {
+            Assert.assertTrue(data.contains("/cricket-helmets-shop-online-india"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void verifyCricketHelmets(){
+        try {
+            String data=helper.getText(ProtectiveGearLocators.verifyCricketHelmets, "Verified ");
+            Assert.assertEquals(data, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void ClickOnInnerThighPads(){
+        helper.clickOnElement(ProtectiveGearLocators.InnerThighPads, "Clicked On InnerThighPads");
+    }
+    public void verifyInnerThighPadsURL(){
+        String data= Base.driver.getCurrentUrl();
+        try {
+            Assert.assertTrue(data.contains("/cricket-inner-thigh-pads-shop-online-india"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void verifyInnerThighPads(){
+        try {
+            String data=helper.getText(ProtectiveGearLocators.VerifyInnerThighPads, "Verified ");
+            Assert.assertEquals(data, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- Project/src/test/java/uistore/ProtectiveGearLocators.java
+++ Project/src/test/java/uistore/ProtectiveGearLocators.java
@@ -10,8 +10,12 @@ public class ProtectiveGearLocators {
     public static By BattingegGuards=By.cssSelector("a[href='https://sportsjam.in/sports/cricket-equipment-store-online-india/cricket-protective-gear-shop-online-india/batting-leg-guards-shop-online-india']>span.name");
     public static By verigyBattinglegGuards=By.xpath("(//span[text()='Batting Leg Guards'])[2]");
     public static By wicketKeepingGloves=By.xpath("//span[text()='Wicket Keeping Gloves']");
+    public static By VerifyWicketKeepingGlove=By.xpath("(//span[text()='Wicket Keeping Gloves'])[2]");
     public static By wicketKeepingLegGuards=By.xpath("//span[text()='Wicket Keeping Leg Guards']");
+    public static By verifyWicketKeepingLegGuard=By.xpath("(//span[text()='Wicket Keeping Leg Guards'])[2]");
     public static By cricketHelmets=By.xpath("//span[text()='Cricket Helmets']");
+    public static By verifyCricketHelmets=By.xpath("(//span[text()='Cricket Helmets'])[2]");
     public static By InnerThighPads=By.xpath("//span[text()='Inner Thigh Pads']");
+    public static By VerifyInnerThighPads=By.xpath("(//span[text()='Inner Thigh Pads'])[2]");
 
 }
